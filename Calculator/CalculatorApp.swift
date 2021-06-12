12bf2915f8fada4abcb8c062e295e4acbb139e4e
Victor Hugo <pavictorhugo@gmail.com>
2021-06-12 14:33:57 -0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Victor Hugo on 12/06/21.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
